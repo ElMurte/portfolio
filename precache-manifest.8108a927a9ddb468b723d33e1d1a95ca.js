dae7f100cc55f88cd1e3b9e0315e4d400b456d37
470M94 <32709092+ElMurte@users.noreply.github.com>
2020-07-12 19:18:13 +0200

    Updates

---
+++ precache-manifest.8108a927a9ddb468b723d33e1d1a95ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f00af8a17af43d3e301a17110ee85f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "de83032f47f78448512a",
+    "url": "/Portfolio/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "f2cdb74bca240c506cd6",
+    "url": "/Portfolio/static/js/2.355ee973.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.355ee973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de83032f47f78448512a",
+    "url": "/Portfolio/static/js/main.ae13049a.chunk.js"
+  },
+  {
+    "revision": "02cf00c0ec98fa9aa891",
+    "url": "/Portfolio/static/js/runtime-main.7d4f3dd6.js"
+  }
+]);
